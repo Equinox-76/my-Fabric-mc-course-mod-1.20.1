46c50eb7b15c0ae74be2b598f44b33dfe5e4616e
Equinox-76 <redstonej318@gmail.com>
2024-10-20 20:00:50 -0400

    Added new items and blocks.

--- src/main/java/net/justin/mccourse/MCCourseMod.java
+++ src/main/java/net/justin/mccourse/MCCourseMod.java
@@ -2,6 +2,8 @@ package net.justin.mccourse;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.justin.mccourse.block.ModBlocks;
+import net.justin.mccourse.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,6 +18,7 @@ public class MCCourseMod implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
+		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
 	}
 }
---
+++ src/main/java/net/justin/mccourse/item/ModItems.java
@@ -0,0 +1,35 @@
+package net.justin.mccourse.item;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.justin.mccourse.MCCourseMod;
+import net.justin.mccourse.block.ModBlocks;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+    public static final Item PINK_GARNET = registerItem("pink_garnet",
+            new Item(new FabricItemSettings()));
+    public static final Item RAW_PINK_GARNET = registerItem("raw_pink_garnet",
+            new Item(new FabricItemSettings()));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, new Identifier(MCCourseMod.MOD_ID, name), item);
+    }
+
+    public static void itemGroupIngredients(FabricItemGroupEntries entries) {
+        entries.add(PINK_GARNET);
+        entries.add(RAW_PINK_GARNET);
+        entries.add(ModBlocks.PINK_GARNET_BLOCK);
+        entries.add(ModBlocks.RAW_PINK_GARNET_BLOCK);
+    }
+    public static void registerModItems() {
+        MCCourseMod.LOGGER.info("Registering Mod Items for " + MCCourseMod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(ModItems::itemGroupIngredients);
+    }
+}
